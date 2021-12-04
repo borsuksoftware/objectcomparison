6faa4534e9cfae3f2d5ac8ab1cce5861b9d1cf73
Steven <steven@borsuksoftware.co.uk>
2021-12-04 16:11:31 +0000

    #3 - Add support for enum comparer plugin

---
+++ BorsukSoftware.Testing.Comparison.Core.Tests/Plugins/EnumComparerPluginTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+
+namespace BorsukSoftware.Testing.Comparison.Plugins
+{
+    public class EnumComparerPluginTests
+    {
+        [InlineData(PlatformID.Win32NT, PlatformID.Win32NT, true)]
+        [InlineData(PlatformID.Win32NT, PlatformID.Unix, false)]
+
+        [Theory]
+        public void Standard(object expected, object actual, bool expectSuccess)
+        {
+            var plugin = new EnumComparerPlugin();
+
+            var response = plugin.TryCompare("bob", expected, actual);
+            if (expectSuccess)
+            {
+                Assert.Equal(ComparisonResultType.Equal, response.ComparisonResultType);
+            }
+            else
+            {
+                Assert.Equal(ComparisonResultType.Different, response.ComparisonResultType);
+            }
+        }
+
+        [InlineData((int)PlatformID.Win32NT, PlatformID.Win32NT)]
+        [Theory]
+        public void DifferentTypes(object expected, object actual)
+        {
+            var plugin = new EnumComparerPlugin();
+
+            var response = plugin.TryCompare("bob", expected, actual);
+            Assert.Equal(ComparisonResultType.UnableToCompare, response.ComparisonResultType);
+        }
+    }
+}
---
+++ BorsukSoftware.Testing.Comparison.Core/Plugins/EnumComparerPlugin.cs
@@ -0,0 +1,35 @@
+﻿namespace BorsukSoftware.Testing.Comparison.Plugins
+{
+    /// <summary>
+    /// Comparer plugin for enums
+    /// </summary>
+    /// <remarks>This plugin doesn't support the case where there's the comparison between an enum and its underlying integer type. 
+    /// This is by design as we would view them as different values</remarks>
+    public class EnumComparerPlugin : IObjectComparerPlugin
+    {
+        public ObjectComparerPluginResults TryCompare(
+            string key,
+            object expected,
+            object actual)
+        {
+            if (expected == null || !expected.GetType().IsEnum ||
+                actual == null || !actual.GetType().IsEnum ||
+                actual.GetType() != expected.GetType())
+                return new BorsukSoftware.Testing.Comparison.ObjectComparerPluginResults
+                {
+                    ComparisonResultType = BorsukSoftware.Testing.Comparison.ComparisonResultType.UnableToCompare,
+                };
+
+            if (actual.Equals(expected))
+                return new BorsukSoftware.Testing.Comparison.ObjectComparerPluginResults
+                {
+                    ComparisonResultType = BorsukSoftware.Testing.Comparison.ComparisonResultType.Equal
+                };
+
+            return new BorsukSoftware.Testing.Comparison.ObjectComparerPluginResults
+            {
+                ComparisonResultType = BorsukSoftware.Testing.Comparison.ComparisonResultType.Different
+            };
+        }
+    }
+}
